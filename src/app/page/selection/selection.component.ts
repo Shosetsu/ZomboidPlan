8d0f1c927e6c9c96c679577d0fb8d5e52224b619
Shosetsu <Shosetsu.rr@outlook.com>
2023-09-16 20:45:30 +0800

    better performance(not best)

--- src/app/page/selection/selection.component.ts
+++ src/app/page/selection/selection.component.ts
@@ -31,12 +31,19 @@ export class SelectionComponent {
 
   selection: Selection[] = selection;
 
-  get scoreStyle() {
-    return {
-      'background-color':
-        'hsl(' + (2 + 200 * (this.score / 100)) + ', 80%, 42%)',
-    };
-  }
+  score =
+    70 +
+    this.selection
+      .map((item) =>
+        item.selection
+          .filter((card) => card.checked)
+          .reduce((p, c) => (p += c.point), 0)
+      )
+      .reduce((p, c) => (p += c), 0);
+
+  scoreStyle = {
+    'background-color': 'hsl(' + (2 + 200 * (this.score / 100)) + ', 80%, 42%)',
+  };
 
   get isCompeleted(): boolean {
     return (
@@ -44,19 +51,6 @@ export class SelectionComponent {
       this.selection.every((item) =>
         item.selection.some((card) => card.checked)
       )
-    );
-  }
-
-  get score(): number {
-    return (
-      70 +
-      this.selection
-        .map((item) =>
-          item.selection
-            .filter((card) => card.checked)
-            .reduce((p, c) => (p += c.point), 0)
-        )
-        .reduce((p, c) => (p += c), 0)
     );
   }
 
@@ -71,5 +65,20 @@ export class SelectionComponent {
           card.aganist?.includes(loopItem.name) && (loopItem.checked = false)
       );
     }
+
+    // re-calculate
+    this.score =
+      70 +
+      this.selection
+        .map((item) =>
+          item.selection
+            .filter((card) => card.checked)
+            .reduce((p, c) => (p += c.point), 0)
+        )
+        .reduce((p, c) => (p += c), 0);
+    this.scoreStyle = {
+      'background-color':
+        'hsl(' + (2 + 200 * (this.score / 100)) + ', 80%, 42%)',
+    };
   }
 }
